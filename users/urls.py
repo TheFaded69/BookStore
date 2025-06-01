07fb662f8d84206fa5a311820574031c8c782cd9
David Bidenko <d.bidenko@wis.software>
2025-06-01 12:57:49 +0300

    Добавление .venv и requirements.txt

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', next_page='/books/book_store/'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/books/book_store/'), name='logout'),
-
 ]
